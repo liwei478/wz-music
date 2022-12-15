e8727d9ad283b07ff877c243bf7b00bae0456e23
lwz <1055824818@qq.com>
2022-12-16 07:49:04 +0800

    实现路由懒加载

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import { useRoutes, Link } from 'react-router-dom'
 import routes from './router'
 
 function App() {
   return (
     <div className="App">
-      <div className="main">{useRoutes(routes)}</div>
+      <div className="nav">
+        <Link to="/discover">发现音乐</Link>
+        <Link to="/mine">我的音乐</Link>
+        <Link to="/focus">关注</Link>
+        <Link to="/download">下载</Link>
+      </div>
+      <Suspense fallback="">
+        <div className="main">{useRoutes(routes)}</div>
+      </Suspense>
     </div>
   )
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import Discover from '@/views/discover'
+import React, { lazy } from 'react'
 import { Navigate, RouteObject } from 'react-router-dom'
-import Focus from '@/views/focus'
-import Mine from '@/views/mine'
-import Download from '@/views/download'
+
+// 所有的路由都会打包到一个文件
+// import Discover from '@/views/discover'
+// import Focus from '@/views/focus'
+// import Mine from '@/views/mine'
+// import Download from '@/views/download'
+
+// 路由懒加载及分包处理
+const Discover = lazy(() => import('@/views/discover'))
+const Mine = lazy(() => import('@/views/mine'))
+const Focus = lazy(() => import('@/views/focus'))
+const Download = lazy(() => import('@/views/download'))
 
 const routes: RouteObject[] = [
   {
