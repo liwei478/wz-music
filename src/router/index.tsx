6cf0b850e05dd2cc7346c1234c12743671e9e899
lwz <1055824818@qq.com>
2022-12-16 19:46:01 +0800

    配置二级路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -12,6 +12,12 @@ const Discover = lazy(() => import('@/views/discover'))
 const Mine = lazy(() => import('@/views/mine'))
 const Focus = lazy(() => import('@/views/focus'))
 const Download = lazy(() => import('@/views/download'))
+const Recommend = lazy(() => import('@/views/discover/c-views/recommend'))
+const Ranking = lazy(() => import('@/views/discover/c-views/ranking'))
+const Songs = lazy(() => import('@/views/discover/c-views/songs'))
+const Djradio = lazy(() => import('@/views/discover/c-views/djradio'))
+const Artist = lazy(() => import('@/views/discover/c-views/artist'))
+const Album = lazy(() => import('@/views/discover/c-views/album'))
 
 const routes: RouteObject[] = [
   {
@@ -20,7 +26,37 @@ const routes: RouteObject[] = [
   },
   {
     path: '/discover',
-    element: <Discover />
+    element: <Discover />,
+    children: [
+      {
+        path: '/discover',
+        element: <Navigate to="/discover/recommend" />
+      },
+      {
+        path: '/discover/recommend',
+        element: <Recommend />
+      },
+      {
+        path: '/discover/ranking',
+        element: <Ranking />
+      },
+      {
+        path: '/discover/songs',
+        element: <Songs />
+      },
+      {
+        path: '/discover/djradio',
+        element: <Djradio />
+      },
+      {
+        path: '/discover/artist',
+        element: <Artist />
+      },
+      {
+        path: '/discover/album',
+        element: <Album />
+      }
+    ]
   },
   {
     path: '/focus',
--- src/views/discover/index.tsx
+++ src/views/discover/index.tsx
@@ -1,12 +1,27 @@
-import React, { memo } from 'react'
+import React, { memo, Suspense } from 'react'
 import type { FC, ReactNode } from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 interface IProps {
   children?: ReactNode
 }
 
 const Discover: FC<IProps> = () => {
-  return <div>Discover</div>
+  return (
+    <div>
+      <div className="nav">
+        <Link to="/discover/recommend">推荐</Link>
+        <Link to="/discover/ranking">歌曲排行</Link>
+        <Link to="/discover/songs">歌曲</Link>
+        <Link to="/discover/djradio">电台</Link>
+        <Link to="/discover/artist">歌手</Link>
+        <Link to="/discover/album">新碟上架</Link>
+      </div>
+      <Suspense fallback="">
+        <Outlet />
+      </Suspense>
+    </div>
+  )
 }
 
 export default memo(Discover)
