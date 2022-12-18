85c59f245180ef22c97c8955686e101d0fa25703
lwz <1055824818@qq.com>
2022-12-18 21:57:42 +0800

    发送网络请求

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,12 +1,45 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import hyRequest from '@/service'
 
 interface IProps {
   children?: ReactNode
 }
 
+interface IBannerData {
+  imageUrl: string
+  targetId: number
+  adid: null
+  targetType: number
+  titleColor: string
+  typeTitle: string
+  url: string
+  scm: string
+  bannerBizType: string
+}
+
 const Recommend: FC<IProps> = () => {
-  return <div>Recommend</div>
+  const [banners, setBanners] = useState<IBannerData[]>([])
+  // 测试网络请求
+  useEffect(() => {
+    hyRequest
+      .get({
+        url: '/banner'
+      })
+      .then((res) => {
+        setBanners(res.banners)
+      })
+  }, [])
+
+  return (
+    <div>
+      <ul>
+        {banners.map((item, index) => (
+          <li key={index}>{item.imageUrl}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default memo(Recommend)
