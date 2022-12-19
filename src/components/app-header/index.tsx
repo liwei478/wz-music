bda2bdfda256e20ef510153b0e0a3374db4a7954
lwz <1055824818@qq.com>
2022-12-19 19:45:09 +0800

    使用 styled-components 搭建样式

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
+import { HeaderWrapper } from './style'
 
 interface IProps {
   children?: ReactNode
@@ -8,12 +9,14 @@ interface IProps {
 
 const AppHeader: FC<IProps> = () => {
   return (
-    <div className="nav">
-      <Link to="/discover">发现音乐</Link>
-      <Link to="/mine">我的音乐</Link>
-      <Link to="/focus">关注</Link>
-      <Link to="/download">下载</Link>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <Link to="/discover">发现音乐</Link>
+        <Link to="/mine">我的音乐</Link>
+        <Link to="/focus">关注</Link>
+        <Link to="/download">下载</Link>
+      </div>
+    </HeaderWrapper>
   )
 }
 
