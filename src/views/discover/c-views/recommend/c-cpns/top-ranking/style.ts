4151129773b5987b0b331fcab3248332d3ef3243
lwz <1055824818@qq.com>
2022-12-23 19:41:40 +0800

    完成榜单页面的编写

--- src/views/discover/c-views/recommend/c-cpns/top-ranking/style.ts
+++ src/views/discover/c-views/recommend/c-cpns/top-ranking/style.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const RankingWrapper = styled.div`
   .content {
+    display: flex;
     height: 472px;
     margin-top: 20px;
     background: url(${require('@/assets/img/recommend-top-bg.png')});
