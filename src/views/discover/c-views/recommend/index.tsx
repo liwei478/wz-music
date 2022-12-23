50c79f3031db046176dbe6e89342e8453065e686
lwz <1055824818@qq.com>
2022-12-23 21:42:30 +0800

    完成右侧登录页面

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -13,6 +13,7 @@ import { RecommendWrapper } from './style'
 import HotRecommend from './c-cpns/hot-recommend'
 import NewAlbum from './c-cpns/new-album'
 import TopRanking from './c-cpns/top-ranking'
+import UserLogin from './c-cpns/user-login'
 
 interface IProps {
   children?: ReactNode
@@ -39,7 +40,11 @@ const Recommend: FC<IProps> = () => {
           <NewAlbum />
           <TopRanking />
         </div>
-        <div className="right">right</div>
+        <div className="right">
+          <UserLogin />
+          <div>歌手入驻</div>
+          <div>热门主播</div>
+        </div>
       </div>
     </RecommendWrapper>
   )
